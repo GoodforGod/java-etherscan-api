63f8909788f6bb48f91e5451bff71a9eda9e5ab3
Anton Kurako <goodforgod.dev@gmail.com>
2023-05-14 23:12:27 +0300

    [2.0.0-SNAPSHOT] 1010L->1015L reset time

--- src/main/java/io/goodforgod/api/etherscan/manager/RequestQueueManager.java
+++ src/main/java/io/goodforgod/api/etherscan/manager/RequestQueueManager.java
@@ -16,16 +16,16 @@ public interface RequestQueueManager extends AutoCloseable {
     /**
      * Is used by default when no API KEY is provided
      */
-    RequestQueueManager ANONYMOUS = new SemaphoreRequestQueueManager(1, Duration.ofMillis(5010L));
+    RequestQueueManager ANONYMOUS = new SemaphoreRequestQueueManager(1, Duration.ofMillis(5015L));
 
     /**
      * Is available for all registered free API KEYs
      * <a href="https://docs.etherscan.io/getting-started/viewing-api-usage-statistics">Free API KEY</a>
      */
-    RequestQueueManager FREE_PLAN = new SemaphoreRequestQueueManager(5, Duration.ofMillis(1010L));
-    RequestQueueManager STANDARD_PLAN = new SemaphoreRequestQueueManager(10, Duration.ofMillis(1010L));
-    RequestQueueManager ADVANCED_PLAN = new SemaphoreRequestQueueManager(20, Duration.ofMillis(1010L));
-    RequestQueueManager PROFESSIONAL_PLAN = new SemaphoreRequestQueueManager(30, Duration.ofMillis(1010L));
+    RequestQueueManager FREE_PLAN = new SemaphoreRequestQueueManager(5, Duration.ofMillis(1015L));
+    RequestQueueManager STANDARD_PLAN = new SemaphoreRequestQueueManager(10, Duration.ofMillis(1015L));
+    RequestQueueManager ADVANCED_PLAN = new SemaphoreRequestQueueManager(20, Duration.ofMillis(1015L));
+    RequestQueueManager PROFESSIONAL_PLAN = new SemaphoreRequestQueueManager(30, Duration.ofMillis(1015L));
 
     RequestQueueManager UNLIMITED = new FakeRequestQueueManager();
 
