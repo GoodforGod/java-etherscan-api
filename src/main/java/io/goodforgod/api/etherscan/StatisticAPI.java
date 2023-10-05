519c26ae22fdc79e76aab423000b76be91bef339
Anton Kurako <goodforgod.dev@gmail.com>
2023-10-06 02:15:47 +0300

    [2.1.0-SNAPSHOT] Javadoc fixed

--- src/main/java/io/goodforgod/api/etherscan/StatisticAPI.java
+++ src/main/java/io/goodforgod/api/etherscan/StatisticAPI.java
@@ -17,7 +17,7 @@ public interface StatisticAPI {
     /**
      * ERC20 token total Supply
      * <a href=
-     * "https://docs.etherscan.io/api-endpoints/tokens#get-erc20-token-totalsupply-by-contractaddress">EtherScan<a>
+     * "https://docs.etherscan.io/api-endpoints/tokens#get-erc20-token-totalsupply-by-contractaddress">EtherScan</a>
      * Returns the current amount of an ERC-20 token in circulation.
      *
      * @param contract contract address
